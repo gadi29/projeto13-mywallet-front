27c305a5cc2372d873dab981bd5307a798734ab8
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-06-28 17:25:44 -0300

    implement main screen

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,105 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 function Main() {
   const navigate = useNavigate();
   let userStorage = localStorage.getItem("user");
 
-  useEffect((() => {
+  /*useEffect((() => {
     if (userStorage === null) {
       return navigate('/login');
     }
-  }), [navigate, userStorage]);
+  }), [navigate, userStorage]);*/
 
   return (
-    <>
-    <h1>TESTE MAIN</h1>
-    </>
+      <Container>
+        <Top>
+          <h1>Olá, Fulano</h1>
+          <ion-icon name="exit-outline"></ion-icon>
+        </Top>
+        <Center>
+
+        </Center>
+        <Bottom>
+          <Link to={'/new_entry'}>
+            <button>
+              <ion-icon name="add-circle-outline"></ion-icon>
+              Nova entrada
+            </button>
+          </Link>
+          <Link to={'/new_exit'}>
+            <button>
+              <ion-icon name="remove-circle-outline"></ion-icon>
+              Nova saída
+            </button>
+          </Link>
+        </Bottom>
+      </Container>
   )
 }
+
+const Container = styled.div`
+  width: 100%;
+  padding: 25px;
+`;
+
+const Top = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  h1 {
+    font-size: 26px;
+    font-weight: 700;
+    color: #FFFFFF;
+  }
+
+  ion-icon {
+    font-size: 30px;
+    color: #FFFFFF;
+    cursor: pointer;
+  }
+`;
+
+const Center = styled.div`
+  background-color: #FFFFFF;
+  border-radius: 5px;
+
+  width: 326px;
+  height: 446px;
+  margin-top: 22px;
+  margin-bottom: 13px;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  button {
+    background-color: #A328D6;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+
+    width: 155px;
+    height: 114px;
+    padding: 10px 0;
+    padding-left: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+
+    color: #FFFFFF;
+    font-size: 17px;
+    font-weight: 700;
+
+    ion-icon {
+      font-size: 24px;
+    }
+  }
+`;
 
 export default Main;
